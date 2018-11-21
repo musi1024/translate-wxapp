c2787410c2e5d842c27a805fab47d281e8db8436
musi <957028613@qq.com>
2018-11-22 02:13:34 +0800

    添加清除全部历史记录功能

--- pages/history/history.js
+++ pages/history/history.js
@@ -10,7 +10,21 @@ Page({
   },
   onTapItem: function(e) {
     wx.reLaunch({
-      url: `/pages/index/index?query=${e.currentTarget.dataset.query}`
+      url: `/pages/index/index?query=${e.currentTarget.dataset.query}&langId=${e.currentTarget.dataset.langid}`
+    })
+    console.log(e.currentTarget.dataset.langid)
+  },
+  onClearAll: function() {
+    let _this = this
+    wx.showModal({
+      title: '提示',
+      content: ' 确定清除全部记录?',
+      success(res) {
+        if (res.confirm) {
+          wx.removeStorageSync('history')
+          _this.setData({history: []}) 
+        }
+      }
     })
   }
 })
