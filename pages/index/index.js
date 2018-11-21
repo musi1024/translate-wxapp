d74c9becd3a85f0fbfd0ce669fc8d783962dcfd2
musi <957028613@qq.com>
2018-11-21 23:11:56 +0800

    完善保存历史功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,36 +1,72 @@
 //index.js
 //获取应用实例
-import { translate } from '../../utils/api.js'
+import {
+  translate
+} from '../../utils/api.js'
 const app = getApp()
 
 Page({
   data: {
     query: '',
+    oldQuery: '',
     hideClearIcon: true,
     result: [],
     curLang: {}
   },
+  onLoad: function(options) {
+    this.setData({
+      query: options.query || ''
+    })
+  },
   onShow: function() {
-    if(this.data.curLang.lang !== app.globalData.curLang.lang){
-      this.setData({ curLang: app.globalData.curLang })
+    if (this.data.curLang.lang !== app.globalData.curLang.lang) {
+      this.setData({
+        curLang: app.globalData.curLang
+      })
+      this.onConfirm()
     }
   },
   onInput: function(e) {
-    this.setData({ 'query': e.detail.value })
-    if(this.data.query.length > 0){
-      this.setData({ 'hideClearIcon': false })
+    this.setData({
+      'query': e.detail.value
+    })
+    console.log(e)
+    if (this.data.query.length > 0) {
+      this.setData({
+        'hideClearIcon': false
+      })
     } else {
-      this.setData({ 'hideClearIcon': true }) 
+      this.setData({
+        'hideClearIcon': true
+      })
     }
   },
-  onTapClose: function () {
-    this.setData({ query: '', hideClearIcon: true })
+  onTapClose: function() {
+    this.setData({
+      query: '',
+      hideClearIcon: true
+    })
   },
   onConfirm: function() {
-    if(!this.data.query) return
-    translate(this.data.query, {from: 'auto', to: this.data.curLang.lang}).then(res => {
-      this.setData({ 'result': res.trans_result })
+    if (this.data.query === this.data.oldQuery) {
+      return 
+    } else {
+      this.data.oldQuery = this.data.query
+    } 
+    translate(this.data.query, {
+      from: 'auto',
+      to: this.data.curLang.lang
+    }).then(res => {
+        this.setData({
+          'result': res.trans_result
+        })
+        let history = wx.getStorageSync('history') || []
+        history.unshift({
+          query: this.data.query,
+          result: res.trans_result[0].dst
+        })
+        history.length = history.length > 15 ? 15 : history.length
+        wx.setStorageSync('history', history)
     })
   }
-  
 })
